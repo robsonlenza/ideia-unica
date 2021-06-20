b08222098d1fbfaf41ddea1b6f0ccaf8efa3da0f
Robson Lenza <robson.lenza@midassolutions.com.br>
2021-06-20 19:16:56 -0300

    controle cache API

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,5 +1,13 @@
 function tempo(request, response){
     const dyanmicDate = new Date();
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+
+    //Exemplo consumo API com variável ambiente
+    //const subscribersResponde = fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    //const subscribersRespondeJson = subscribersResponde.json();
+    //const inscritos = subscribersRespondeJson.tota_subscribers;
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
     response.json({
         date: dyanmicDate.toTimeString()
